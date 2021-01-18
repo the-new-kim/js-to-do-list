aa2beb15bc6309a82fb961b0076ce408b643e6c7
the-new-kim <jonkim0309@gmail.com>
2021-01-18 20:36:45 +0900

    what i learned today...

--- greeting.js
+++ greeting.js
@@ -1,6 +1,27 @@
 const form = document.querySelector(".js-form"),
+  greeting = document.querySelector(".js-greetings"),
   input = form.querySelector("input");
 
-function init() {}
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN); //required????
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `HELLO! ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    //form.classList.add(SHOWING_CN);
+  } else {
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
 
 init();
