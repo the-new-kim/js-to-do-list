0afbc03b4b6d4db9eb3668b6af75bc595f68c361
the-new-kim <73030645+the-new-kim@users.noreply.github.com>
2021-01-31 21:11:56 +0900

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,25 +1,49 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
-  tiDoList = document.querySelector(".js-toDoList");
+  toDoList = document.querySelector(".js-toDoList"); //ul tag
 
 const TODOS_LS = "toDos";
 
-function saveToDo() {}
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); //in local storage the datas can be saved only as a string so we need the JSON.stringfy...
+}
 
 function paintingToDo(text) {
-  console.log(text);
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button"); //create elements...
+  const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "X";
+  span.innerText = text; //the value of input will be here
+  li.appendChild(delBtn);
+  li.appendChild(span); //put created elements inside of li tag
+  li.id = newId;
+  toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = toDoInput.value;
   paintingToDo(currentValue);
-  toDoInput.value = ""; //what is this?
+  toDoInput.value = ""; //this removes the inputed text after submit
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedTodos = JSON.parse(loadedToDos);
+    parsedTodos.forEach(function (toDo) {
+      paintingToDo(toDo.text);
+    });
   }
 }
 
