237dbe1d7bde7c76c6c50e6d2ca139e538c46e6a
the-new-kim <73030645+the-new-kim@users.noreply.github.com>
2021-01-31 10:54:11 +0900

    todays practice

--- todo.js
+++ todo.js
@@ -4,12 +4,28 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+function saveToDo() {}
+
+function paintingToDo(text) {
+  console.log(text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintingToDo(currentValue);
+  toDoInput.value = ""; //what is this?
+}
+
 function loadToDos() {
   const toDos = localStorage.getItem(TODOS_LS);
+  if (toDos !== null) {
+  }
 }
 
 function init() {
   loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
