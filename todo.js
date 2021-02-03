18da945e4d2beebf951b2d5508f20faf6a0a9893
the-new-kim <73030645+the-new-kim@users.noreply.github.com>
2021-02-04 00:37:21 +0900

    Update todo.js

--- todo.js
+++ todo.js
@@ -4,7 +4,23 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let idNumbers = 1;
+
+let toDos = [];
+
+function delToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  //toDoList ist the parentNode and li ist the child
+  //this function is recalled of a click event
+  //so only clicked elements parent will be deleted
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
 
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); //in local storage the datas can be saved only as a string so we need the JSON.stringfy...
@@ -14,8 +30,11 @@ function paintingToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button"); //create elements...
   const span = document.createElement("span");
-  const newId = toDos.length + 1;
+  //const newId = toDos.length + 1;
+  const newId = idNumbers;
+  idNumbers += 1;
   delBtn.innerText = "X";
+  delBtn.addEventListener("click", delToDo);
   span.innerText = text; //the value of input will be here
   li.appendChild(delBtn);
   li.appendChild(span); //put created elements inside of li tag
