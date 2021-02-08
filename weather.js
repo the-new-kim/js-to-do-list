0bcc9244fd69377c851a9a81784cd0d1c30804e0
the-new-kim <73030645+the-new-kim@users.noreply.github.com>
2021-02-08 21:24:40 +0900

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,3 +1,40 @@
-function init() {}
+const API_KEY = "4f4ae174dd9b4e994aa13974e09f450c";
+
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("can not access");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    //get weather
+  }
+}
+
+function init() {
+  loadCoords();
+}
 
 init();
